278e2095b21dcf6e0f38f51fb735b85e512598db
neeraj <neerajlajpal@gmail.com>
2018-01-20 19:36:15 +0530

    unocoin new api

--- magic.py
+++ magic.py
@@ -1,23 +1,12 @@
 import datetime
-import json
 import logging
-# from selenium import webdriver
 from threading import Thread
-
 import pymongo
 import requests
-from selenium.webdriver import Firefox
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.support import expected_conditions as expected
-from selenium.webdriver.support.wait import WebDriverWait
-
-# from selenium.webdriver.common.keys import Keys
 
 apiUrlMapping = {"zebpay": "https://www.zebapi.com/api/v1/market/ticker/btc/inr",
                   "koinex": "https://koinex.in/api/ticker",
-                  "unocoin": "https://www.unocoin.com/trade.php?all"}
+                  "unocoin": "https://www.unocoin.com/api/v1/general/prices"}
 logging.basicConfig(filename='magic.log', level=logging.DEBUG)
 results = {"ts": datetime.datetime.now(datetime.timezone.utc)}
 
@@ -31,27 +20,26 @@ class GetDataThread(Thread):
         get_data(self.com)
 
 
+def remove_fields(ret):
+    redundantFields = ['buybtc', 'sellbtc', 'min_24_buy', 'max_24_buy']
+    newRet = {}
+    for r in redundantFields:
+        newRet[r] = ret[r]
+    return newRet
+
+
 def get_data(com):
+    url = apiUrlMapping[com]
     if com == "unocoin":
-        options = Options()
-        options.add_argument('-headless')
-        driver = Firefox(firefox_options=options)
-        wait = WebDriverWait(driver, timeout=10)
-        driver.get(apiUrlMapping[com])
-        wait.until(expected.visibility_of_element_located((By.TAG_NAME, 'body'))).send_keys(
-            'headless firefox' + Keys.ENTER)
-        x = driver.find_element_by_xpath("//html/body").get_attribute('innerHTML')
-        x = json.loads(x)
-        driver.quit()
-        # logging.info((apiUrlMapping[com]) + "     " + str(x))
-        results[com] = x
-        # return x
+        headers = {"Content-Type": "application/json",
+                   "Authorization": "Bearer " + "8ee2ac117dfd5ac86e7646841817460eff4e6f44"}
+        ret = requests.post(url, headers=headers)
+        ret = ret.json()
+        ret = remove_fields(ret)
     else:
-        ret = (requests.get(apiUrlMapping[com]))
-        # print(ret.headers)
-        # logging.info((apiUrlMapping[com]) + "     " + str(ret))
-        results[com] = ret.json()
-        # return ret.json()
+        ret = (requests.get(url))
+        ret = ret.json()
+    results[com] = ret
 
 
 def get_final_data():
@@ -62,25 +50,19 @@ def get_final_data():
         t.start()
     for t in threads:
         t.join()
-
-    # result = get_data(k)
-    #    if result is not None:
-    #        results[k] = result
     return results
 
 
-def do_magic():
-    # from pymongo import MongoClient
-
+def get_mongo_connection():
     client = pymongo.MongoClient()
     db = client.coinExchangeDB
-    collection = db.coinInfo
+    return db.coinInfo
 
+
+def do_magic():
+    collection = get_mongo_connection()
     finalData = get_final_data()
     idd = collection.insert_one(finalData).inserted_id
-
-    # logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
-    # logging.info(finalData["ts"])
     logging.info(str(idd))
 
 
