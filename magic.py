244e8e535cc782ce9f809f4ef2c8a7e322b3929e
hackdaemon2 <neerajlajpal@gmail.com>
2018-03-27 13:12:17 +0530

    fix

--- magic.py
+++ magic.py
@@ -287,10 +287,12 @@ def transform_coinome_data(res):
 def transform_coindelta_data(res):
     ret = []
     for key in res:
+        tmp = {}
         tmp['currency'] = key["MarketName"].lower().split('-')[1]
         if tmp['currency'] == 'inr':
             newkey = (key["MarketName"].lower().split('-')[0] + "__coindelta")
             tmp = fill_coin_data(newkey)
+            tmp['currency'] = key["MarketName"].lower().split('-')[1]
             tmp["cp"] = str(key["Last"])
             ret.append(tmp)
     return ret
