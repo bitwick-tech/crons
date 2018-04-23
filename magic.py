1d554057450907a7fb515a083c2d1f66487fbdbb
hackdaemon2 <neerajlajpal@gmail.com>
2018-04-24 00:53:52 +0530

    coinome inr fix

--- magic.py
+++ magic.py
@@ -375,6 +375,8 @@ def transform_coinome_data(res):
         tmp = fill_coin_data(newkey)
         tmp['currency'] = key.lower().split('-')[1]
         tmp["cp"] = str(val['last'])
+        if tmp['cp'] != 'inr':
+            continue
         ret.append(tmp)
     return ret
 
